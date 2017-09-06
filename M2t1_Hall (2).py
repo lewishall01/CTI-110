5ab88abd0fb3ab989f24559f7dd658e5eb73a8dd
lewishall01 <31700959+lewishall01@users.noreply.github.com>
2017-09-06 11:18:20 -0400

    Add files via upload
    First Submission

---
+++ M2t1_Hall (2).py
@@ -0,0 +1,34 @@
+#Cti 110
+#M2T1- Sales Prediction
+#8/30
+#Hall
+
+#Suggested Variables: totalSales, annualProfit
+
+#ask the user for total sales
+totalSales = int (input ("the total sales is: "))
+
+#calculate the profit amount
+annualProfit = 0.23  * totalSales
+
+#print the profit amount
+print(annualProfit)
+
+
+
+
+'''
+# program to add two numbers
+#usertyped = input ("Type a number: ")
+#usernumber =int (usertyped)
+
+#get the numbers
+firstnumber =int (input("Type a number: "))
+second number= int(input("Type another number:"))
+
+#add them
+answer= firstnumber + second number
+
+#print the result
+print ("The answer is", answer)
+'''
